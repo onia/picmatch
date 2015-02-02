4ea31bc5646688682f37c21605471819becbe3e8
Onia YANG <bitcoin:1CoTtqMc6P1PMgXJrmsWGge2btdAyTxxeE>
2015-02-03 01:28:53 +0800

    fix upload bug

--- picmatch/fileupload/urls.py
+++ picmatch/fileupload/urls.py
@@ -7,11 +7,12 @@ from fileupload.views import (
         )
 
 urlpatterns = patterns('',
-    url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
+    #url(r'^basic/$', BasicVersionCreateView.as_view(), name='upload-basic'),
     url(r'^basic/plus/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
-    url(r'^new/$', PictureCreateView.as_view(), name='upload-new'),
-    url(r'^angular/$', AngularVersionCreateView.as_view(), name='upload-angular'),
-    url(r'^jquery-ui/$', jQueryVersionCreateView.as_view(), name='upload-jquery'),
+    #url(r'^new/$', PictureCreateView.as_view(), name='upload-new'),
+    url(r'^new/$', BasicPlusVersionCreateView.as_view(), name='upload-basic-plus'),
+    #url(r'^angular/$', AngularVersionCreateView.as_view(), name='upload-angular'),
+    #url(r'^jquery-ui/$', jQueryVersionCreateView.as_view(), name='upload-jquery'),
     url(r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), name='upload-delete'),
     url(r'^view/$', PictureListView.as_view(), name='upload-view'),
 )
